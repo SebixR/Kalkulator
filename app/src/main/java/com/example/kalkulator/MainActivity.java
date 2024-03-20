2aa0b5c6feb082773c4ca205e38219fd79ec6018
SebixR <sebarog@gmail.com>
2024-03-20 19:28:35 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -0,0 +1,243 @@
+package com.example.kalkulator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+
+import org.mariuszgromada.math.mxparser.Expression;
+import org.mariuszgromada.math.mxparser.License;
+
+import java.text.DecimalFormat;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView expressionView, resultView;
+
+    private boolean isComplexView = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.normal);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+
+        boolean isComplex = intent.getBooleanExtra("isComplex", false);
+        if (isComplex) {
+            setContentView(R.layout.complex);
+            assignComplexIds();
+            isComplexView = true;
+        }
+
+        assignIds();
+    }
+
+    public void assignId(int id) {
+        MaterialButton button = findViewById(id);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+
+        String buttonText = button.getText().toString();
+        StringBuilder expression = new StringBuilder(expressionView.getText().toString());
+
+        if (button.getId() == R.id.buttonSwitch) {
+            handleSwitch();
+        }
+
+        if (button.getId() == R.id.buttonAC){
+            expression = new StringBuilder();
+            resultView.setText("0");
+        }
+        else if (button.getId() == R.id.buttonSign) {
+            handleSign(expression);
+        }
+        else if (button.getId() == R.id.buttonBack && (expression.length() > 0)) {
+            expression = handleBack(expression);
+        }
+        else if (button.getId() == R.id.buttonEquals) {
+            handleEquals(expression);
+        }
+        else {
+            if (validate(expression.toString(), button.getId())) {
+
+                expression.append(buttonText);
+
+                if (button.getId() == R.id.buttonSin || button.getId() == R.id.buttonCos || button.getId() == R.id.buttonTan
+                        || button.getId() == R.id.buttonLog || button.getId() == R.id.buttonLn) {
+                    expression.append("(");
+                }
+            }
+        }
+
+        expressionView.setText(expression.toString());
+    }
+
+    public void handleSwitch() {
+        if (isComplexView) setContentView(R.layout.normal);
+        else {
+            setContentView(R.layout.complex);
+            assignComplexIds();
+        }
+        isComplexView = !isComplexView;
+        assignIds();
+    }
+
+    public void handleSign(StringBuilder expression) {
+        if (expression.length() > 0) {
+            for (int i = expression.length() - 1; i >= 0; i--) {
+                if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '/'
+                        || expression.charAt(i) == '*' || expression.charAt(i) == '%'  || expression.charAt(i) == '√'
+                        || i == 0) {
+                    if (expression.charAt(i) == '-') expression.setCharAt(i, '+');
+                    else if (expression.charAt(i) == '+') expression.setCharAt(i, '-');
+                    else expression.insert(i, '-');
+                    break;
+                }
+            }
+        }
+    }
+
+    public StringBuilder handleBack(StringBuilder expression) {
+        if (expression.length() >= 4) {
+            String subString = expression.substring(expression.length() - 4);
+            if (subString.equals("sin(") || subString.equals("cos(") || subString.equals("tan(")) {
+                expression = new StringBuilder(expression.substring(0, expression.length() - 4));
+            }
+            else expression = new StringBuilder(expression.substring(0, expression.length() - 1));
+        }
+        else if (expression.length() >= 3) {
+            String subString = expression.substring(expression.length() - 3);
+            if (subString.equals("ln(") || subString.equals("lg(")) {
+                expression = new StringBuilder(expression.substring(0, expression.length() - 3));
+            }
+            else expression = new StringBuilder(expression.substring(0, expression.length() - 1));
+        }
+        else expression = new StringBuilder(expression.substring(0, expression.length() - 1));
+
+        return expression;
+    }
+
+    public void handleEquals(StringBuilder expression) {
+        if (expression.length() > 0) {
+
+            int openBrackets = 0, closedBrackets = 0;
+            for (int i = 0; i < expression.length(); i++){
+                if (expression.charAt(i) == '(') openBrackets++;
+                if (expression.charAt(i) == ')') closedBrackets++;
+            }
+            for (int i = 0; i < openBrackets - closedBrackets; i++) {
+                expression.append(')');
+            }
+
+            String result = calculate(expression.toString());
+            resultView.setText(result);
+        }
+    }
+
+    public boolean validate(String expression, int buttonId) {
+
+        if (!expression.isEmpty()) {
+            boolean isDigit = !Character.isDigit(expression.charAt(expression.length() - 1));
+            if ((buttonId == R.id.buttonAdd || buttonId == R.id.buttonSubtract || buttonId == R.id.buttonMultiply || buttonId == R.id.buttonDivide)
+                && isDigit && expression.charAt(expression.length() - 1) != 'e' && expression.charAt(expression.length() - 1) != ')'
+                    && expression.charAt(expression.length() - 1) != '(' && expression.charAt(expression.length() - 1) != '%') {
+                return false;
+            }
+            else return true;
+        }
+        else {
+            if (buttonId == R.id.buttonAdd || buttonId == R.id.buttonMultiply || buttonId == R.id.buttonDivide
+                    || buttonId == R.id.buttonPercent || buttonId == R.id.buttonFactor || buttonId == R.id.buttonPower) {
+                return false;
+            }
+            else return true;
+        }
+    }
+
+    public String calculate(String expression) {
+        try {
+
+            License.iConfirmNonCommercialUse("Sebastian Rogaczewski");
+            Expression mxExpression = new Expression(expression);
+
+            double number = mxExpression.calculate();
+
+            DecimalFormat df = new DecimalFormat("#.########");
+            String result = df.format(number);
+            if (result.endsWith(".0")) {
+                result = result.substring(0, result.length() - 2);
+            }
+            if (result.equals("NaN")) {
+                return "Error";
+            }
+            return result;
+        } catch (Exception e) {
+            return "Error";
+        }
+    }
+
+    public void assignIds()
+    {
+        resultView = findViewById(R.id.resultView);
+        expressionView = findViewById(R.id.expressionView);
+
+        assignId(R.id.buttonAC);
+        assignId(R.id.buttonBack);
+        assignId(R.id.buttonSign);
+        assignId(R.id.buttonDivide);
+        assignId(R.id.buttonMultiply);
+        assignId(R.id.buttonSubtract);
+        assignId(R.id.buttonAdd);
+        assignId(R.id.buttonSwitch);
+        assignId(R.id.buttonEquals);
+        assignId(R.id.buttonComma);
+
+        assignId(R.id.button1);
+        assignId(R.id.button2);
+        assignId(R.id.button3);
+        assignId(R.id.button4);
+        assignId(R.id.button5);
+        assignId(R.id.button6);
+        assignId(R.id.button7);
+        assignId(R.id.button8);
+        assignId(R.id.button9);
+        assignId(R.id.button0);
+    }
+
+    public void assignComplexIds() {
+        resultView = findViewById(R.id.resultView);
+        expressionView = findViewById(R.id.expressionView);
+
+        assignId(R.id.buttonSin);
+        assignId(R.id.buttonCos);
+        assignId(R.id.buttonTan);
+        assignId(R.id.buttonPi);
+        assignId(R.id.buttonE);
+        assignId(R.id.buttonFactor);
+        assignId(R.id.buttonPower);
+        assignId(R.id.buttonRoot);
+        assignId(R.id.buttonLog);
+        assignId(R.id.buttonLn);
+        assignId(R.id.buttonPercent);
+    }
+}
---
+++ app/src/main/java/com/example/kalkulator/MenuActivity.java
@@ -0,0 +1,59 @@
+package com.example.kalkulator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.menu);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menu), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button exitButton = findViewById(R.id.buttonExit);
+        exitButton.setOnClickListener(v -> {
+            finish();
+            System.exit(0);
+        });
+    }
+
+
+    public void goToNormal(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+
+        boolean isComplex = false;
+        intent.putExtra("isComplex", isComplex);
+
+        startActivity(intent);
+    }
+
+    public void goToAdvanced(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+
+        boolean isComplex = true;
+        intent.putExtra("isComplex", isComplex);
+
+        startActivity(intent);
+    }
+
+    public void goToAbout(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+
+        startActivity(intent);
+    }
+}
